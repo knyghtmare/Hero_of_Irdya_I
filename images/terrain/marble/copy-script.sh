77a162fd803a3e0859e7d4ea7ea5b0748ac2fd88
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-01-10 21:19:45 +0600

    added 4 new terrain types with their graphics
    credits to doofus-01 and Iris

---
+++ images/terrain/marble/copy-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp castle-concave-br.png castle-convex-br.png
+cp castle-concave-bl.png castle-convex-bl.png
+cp castle-concave-r.png castle-convex-r.png
+cp castle-concave-l.png castle-convex-l.png
+cp castle-concave-tr.png castle-convex-tr.png
+cp castle-concave-tl.png castle-convex-tl.png
+cp keep-castle-convex-tl.png keep-castle-cw-tl.png
+cp keep-castle-convex-tr.png keep-castle-cw-tr.png
+cp keep-castle-convex-tl.png keep-castle-ccw-tl.png
+cp keep-castle-convex-tr.png keep-castle-ccw-tr.png
