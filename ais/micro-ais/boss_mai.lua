abe77b0b507f781a5434340f07f7469b31928efd
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-02 21:31:32 +0600

    Update boss_mai.lua

--- ais/micro-ais/boss_mai.lua
+++ ais/micro-ais/boss_mai.lua
@@ -3,7 +3,8 @@ function wesnoth.micro_ais.boss(cfg)
 	    if (not cfg.id) and (not wml.get_child(cfg, "filter")) then
 			wml.error("Boss [micro_ai] tag requires either id= key or [filter] tag")
         end
-        -- We do not need this
+		-- LK: We do not need this
+		-- the boss will just wander, target, kill, and find another target
 		-- AH.get_named_loc_xy('home', cfg, 'Scenario Boss [micro_ai] tag')
 	end
 	local required_keys = {}
