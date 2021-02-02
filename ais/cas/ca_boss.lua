d00378dbbca7124828a075b072b36f0b85c7e739
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-02 20:26:50 +0600

    Update ca_boss.lua

--- ais/cas/ca_boss.lua
+++ ais/cas/ca_boss.lua
@@ -4,7 +4,7 @@ local MAIUV = wesnoth.require "ai/micro_ais/micro_ai_unit_variables.lua"
 local M = wesnoth.map
 
 local function boss_attack_weakest_adj_enemy(ai, boss_unit)
-    -- Attack the enemy with the fewest hitpoints adjacent to 'hunter', if there is one
+    -- Attack the enemy with the fewest hitpoints adjacent to 'boss', if there is one
     -- Returns status of the attack:
     --   'attacked': if a unit was attacked
     --   'killed': if a unit was killed
@@ -51,33 +51,33 @@ function ca_boss:evaluation(cfg)
 end
 
 function ca_boss:execution(cfg)
-    -- Hunter does a random wander in area given by @cfg.hunting_ground until it finds
-    -- and kills an enemy unit, then retreats to position given by @cfg.home_loc or @cfg.home_x/y
-    -- for @cfg.rest_turns turns, or until fully healed
+    -- Scenario Boss does a random wander in area given by @cfg.wandering_ground until it finds
+    -- and kills an enemy unit
+    -- and then rinse and repeat
 
-    local hunter = get_hunter(cfg)
-    local hunter_vars = MAIUV.get_mai_unit_variables(hunter, cfg.ai_id)
+    local boss = get_level_boss(cfg)
+    local boss_vars = MAIUV.get_mai_unit_variables(boss, cfg.ai_id)
 
-    -- If hunting_status is not set for the hunter -> default behavior -> random wander
-    if (not hunter_vars.hunting_status) then
-        -- Hunter gets a new goal if none exist or on any move with 10% random chance
+    -- If boss_status is not set for the boss -> default behavior -> random wander
+    if (not boss_vars.boss_status) then
+        -- boss gets a new goal if none exist or on any move with 10% random chance
         local rand = math.random(10)
-        if (not hunter_vars.goal_x) or (rand == 1) then
+        if (not boss_vars.goal_x) or (rand == 1) then
             -- 'locs' includes border hexes, but that does not matter here
-            locs = AH.get_passable_locations((wml.get_child(cfg, "filter_location") or {}), hunter)
+            locs = AH.get_passable_locations((wml.get_child(cfg, "filter_location") or {}), boss)
             local rand = math.random(#locs)
 
-            hunter_vars.goal_x, hunter_vars.goal_y = locs[rand][1], locs[rand][2]
-            MAIUV.set_mai_unit_variables(hunter, cfg.ai_id, hunter_vars)
+            boss_vars.goal_x, boss_vars.goal_y = locs[rand][1], locs[rand][2]
+            MAIUV.set_mai_unit_variables(boss, cfg.ai_id, boss_vars)
         end
 
-        local reach_map = AH.get_reachable_unocc(hunter)
+        local reach_map = AH.get_reachable_unocc(boss)
 
         -- Now find the one of these hexes that is closest to the goal
         local max_rating, best_hex = - math.huge
         reach_map:iter( function(x, y, v)
             -- Distance from goal is first rating
-            local rating = -M.distance_between(x, y, hunter_vars.goal_x, hunter_vars.goal_y)
+            local rating = -M.distance_between(x, y, boss_vars.goal_x, boss_vars.goal_y)
 
             -- Huge rating bonus if this is next to an enemy
             local enemy_hp = 500
@@ -94,75 +94,75 @@ function ca_boss:execution(cfg)
             end
         end)
 
-        if (best_hex[1] ~= hunter.x) or (best_hex[2] ~= hunter.y) then
-            AH.checked_move(ai, hunter, best_hex[1], best_hex[2])  -- partial move only
-            if (not hunter) or (not hunter.valid) then return end
-        else  -- If hunter did not move, we need to stop it (also delete the goal)
-            AH.checked_stopunit_moves(ai, hunter)
-            if (not hunter) or (not hunter.valid) then return end
-            hunter_vars.goal_x, hunter_vars.goal_y = nil, nil
-            MAIUV.set_mai_unit_variables(hunter, cfg.ai_id, hunter_vars)
+        if (best_hex[1] ~= boss.x) or (best_hex[2] ~= boss.y) then
+            AH.checked_move(ai, boss, best_hex[1], best_hex[2])  -- partial move only
+            if (not boss) or (not boss.valid) then return end
+        else  -- If boss did not move, we need to stop it (also delete the goal)
+            AH.checked_stopunit_moves(ai, boss)
+            if (not boss) or (not boss.valid) then return end
+            boss_vars.goal_x, boss_vars.goal_y = nil, nil
+            MAIUV.set_mai_unit_variables(boss, cfg.ai_id, boss_vars)
         end
 
-        -- If this gets the hunter to the goal, we delete the goal
-        if (hunter.x == hunter_vars.goal_x) and (hunter.y == hunter_vars.goal_y) then
-            hunter_vars.goal_x, hunter_vars.goal_y = nil, nil
-            MAIUV.set_mai_unit_variables(hunter, cfg.ai_id, hunter_vars)
+        -- If this gets the boss to the goal, we delete the goal
+        if (boss.x == boss_vars.goal_x) and (boss.y == boss_vars.goal_y) then
+            boss_vars.goal_x, boss_vars.goal_y = nil, nil
+            MAIUV.set_mai_unit_variables(boss, cfg.ai_id, boss_vars)
         end
 
-        -- Finally, if the hunter ended up next to enemies, attack the weakest of those
-        local attack_status = hunter_attack_weakest_adj_enemy(ai, hunter)
+        -- Finally, if the boss ended up next to enemies, attack the weakest of those
+        local attack_status = boss_attack_weakest_adj_enemy(ai, boss)
 
-        -- If the enemy was killed, hunter returns home
-        if hunter and hunter.valid and (attack_status == 'killed') then
-            hunter_vars.goal_x, hunter_vars.goal_y = nil, nil
-            hunter_vars.hunting_status = 'returning'
-            MAIUV.set_mai_unit_variables(hunter, cfg.ai_id, hunter_vars)
+        -- If the enemy was killed, boss returns home
+        if boss and boss.valid and (attack_status == 'killed') then
+            boss_vars.goal_x, boss_vars.goal_y = nil, nil
+            boss_vars.boss_status = 'returning'
+            MAIUV.set_mai_unit_variables(boss, cfg.ai_id, boss_vars)
 
             if cfg.show_messages then
-                wesnoth.wml_actions.message { speaker = hunter.id, message = 'Now that I have eaten, I will go back home.' }
+                wesnoth.wml_actions.message { speaker = boss.id, message = 'Now that I have eaten, I will go back home.' }
             end
         end
 
         return
     end
 
-    -- If we got here, this means the hunter is either returning, or resting
-    if (hunter_vars.hunting_status == 'returning') then
+    -- If we got here, this means the boss is either returning, or resting
+    if (boss_vars.boss_status == 'returning') then
         local home_loc = AH.get_named_loc_xy('home', cfg)
-        goto_x, goto_y = wesnoth.find_vacant_tile(home_loc[1], home_loc[2], hunter)
+        goto_x, goto_y = wesnoth.find_vacant_tile(home_loc[1], home_loc[2], boss)
 
-        local next_hop = AH.next_hop(hunter, goto_x, goto_y)
+        local next_hop = AH.next_hop(boss, goto_x, goto_y)
         if next_hop then
-            AH.movefull_stopunit(ai, hunter, next_hop)
-            if (not hunter) or (not hunter.valid) then return end
+            AH.movefull_stopunit(ai, boss, next_hop)
+            if (not boss) or (not boss.valid) then return end
 
             -- If there's an enemy on the 'home' hex and we got right next to it, attack that enemy
-            if (M.distance_between(home_loc[1], home_loc[2], hunter.x, hunter.y) == 1) then
+            if (M.distance_between(home_loc[1], home_loc[2], boss.x, boss.y) == 1) then
                 local enemy = wesnoth.units.get(home_loc[1], home_loc[2])
                 if AH.is_attackable_enemy(enemy) then
                     if cfg.show_messages then
-                        wesnoth.wml_actions.message { speaker = hunter.id, message = 'Get out of my home!' }
+                        wesnoth.wml_actions.message { speaker = boss.id, message = 'Get out of my home!' }
                     end
 
-                    AH.checked_attack(ai, hunter, enemy)
-                    if (not hunter) or (not hunter.valid) then return end
+                    AH.checked_attack(ai, boss, enemy)
+                    if (not boss) or (not boss.valid) then return end
                 end
             end
         end
 
         -- We also attack the weakest adjacent enemy, if still possible
-        hunter_attack_weakest_adj_enemy(ai, hunter)
-        if (not hunter) or (not hunter.valid) then return end
+        boss_attack_weakest_adj_enemy(ai, boss)
+        if (not boss) or (not boss.valid) then return end
 
-        -- If the hunter got home, start the resting counter
-        if (hunter.x == home_loc[1]) and (hunter.y == home_loc[2]) then
-            hunter_vars.hunting_status = 'resting'
-            hunter_vars.resting_until = wesnoth.current.turn + (cfg.rest_turns or 3)
-            MAIUV.set_mai_unit_variables(hunter, cfg.ai_id, hunter_vars)
+        -- If the boss got home, start the resting counter
+        if (boss.x == home_loc[1]) and (boss.y == home_loc[2]) then
+            boss_vars.boss_status = 'resting'
+            boss_vars.resting_until = wesnoth.current.turn + (cfg.rest_turns or 3)
+            MAIUV.set_mai_unit_variables(boss, cfg.ai_id, boss_vars)
 
             if cfg.show_messages then
-                wesnoth.wml_actions.message { speaker = hunter.id, message = 'I made it home - resting now until the end of Turn ' .. hunter_vars.resting_until .. ' or until fully healed.' }
+                wesnoth.wml_actions.message { speaker = boss.id, message = 'I made it home - resting now until the end of Turn ' .. boss_vars.resting_until .. ' or until fully healed.' }
             end
         end
 
@@ -170,31 +170,31 @@ function ca_boss:execution(cfg)
     end
 
     -- If we got here, the only remaining action should be resting
-    if (hunter_vars.hunting_status == 'resting') then
-        AH.checked_stopunit_moves(ai, hunter)
-        if (not hunter) or (not hunter.valid) then return end
+    if (boss_vars.boss_status == 'resting') then
+        AH.checked_stopunit_moves(ai, boss)
+        if (not boss) or (not boss.valid) then return end
 
         -- We also attack the weakest adjacent enemy
-        hunter_attack_weakest_adj_enemy(ai, hunter)
-        if (not hunter) or (not hunter.valid) then return end
+        boss_attack_weakest_adj_enemy(ai, boss)
+        if (not boss) or (not boss.valid) then return end
 
         -- If this is the last turn of resting, we remove the status and turn variable
-        if (hunter.hitpoints >= hunter.max_hitpoints) and (hunter_vars.resting_until <= wesnoth.current.turn) then
-            hunter_vars.hunting_status = nil
-            hunter_vars.resting_until = nil
-            MAIUV.set_mai_unit_variables(hunter, cfg.ai_id, hunter_vars)
+        if (boss.hitpoints >= boss.max_hitpoints) and (boss_vars.resting_until <= wesnoth.current.turn) then
+            boss_vars.boss_status = nil
+            boss_vars.resting_until = nil
+            MAIUV.set_mai_unit_variables(boss, cfg.ai_id, boss_vars)
 
             if cfg.show_messages then
-                wesnoth.wml_actions.message { speaker = hunter.id, message = 'I am done resting. It is time to go hunting again next turn.' }
+                wesnoth.wml_actions.message { speaker = boss.id, message = 'I am done resting. It is time to go boss again next turn.' }
             end
         end
         return
     end
 
     -- In principle we should never get here, but just in case something got changed in WML:
-    -- Reset variable, so that hunter goes hunting on next turn
-    hunter_vars.hunting_status = nil
-    MAIUV.set_mai_unit_variables(hunter, cfg.ai_id, hunter_vars)
+    -- Reset variable, so that boss goes boss on next turn
+    boss_vars.boss_status = nil
+    MAIUV.set_mai_unit_variables(boss, cfg.ai_id, boss_vars)
 end
 
 return ca_boss
