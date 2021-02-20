6ee0c942f311358ee7fd76c31c43981a5ed372a2
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-02-20 22:27:02 +0600

    Update chat_jahin.lua

--- lua/scenarios/chat_jahin.lua
+++ lua/scenarios/chat_jahin.lua
@@ -5,3 +5,9 @@ function chat_jahin(t)
     -- speaker is Jahin, by default
     wesnoth.fire("chat", { speaker = "Jahin", message = t.txt_msg })
 end
+
+function narrator_message_mysterious(txt)
+    -- behave like a message tag
+    -- with speaker fixed
+    wesnoth.fire("message", { speaker = narrator, image=, message = txt.txt_msg })
+end
